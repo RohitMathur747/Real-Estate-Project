77db53b8e2ae30713c3048957cb3b2ec23ca1801
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-01-22 22:28:52 +0530

    Real Estate Project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Navbar.scss";
+
+const navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="logo" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default navbar;
