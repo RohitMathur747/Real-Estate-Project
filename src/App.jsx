46abbef20d8bb37690a918dad6f87f3faed91408
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-01-23 22:54:56 +0530

    Day 2 - Ui Building - upto 1:00 hr

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import "./layout.scss";
-import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./routes/Homepage/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ListPage from "./routes/listPage/ListPage";
+import Layout from "./routes/Layout/Layout";
+//import SinglePage from "./routes/singlePage/singlePage";
+//import ProfilePage from "./routes/profilePage/profilePage";
+//import Login from "./routes/login/login";
+//import Register from "./routes/register/register";
 
 function App() {
-  return (
-    <div className="layout">
-      <Navbar />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        // {
+        //   path: "/:id",
+        //   element: <SinglePage />,
+        // },
+        // {
+        //   path: "/profile",
+        //   element: <ProfilePage />,
+        // },
+        // {
+        //   path: "/login",
+        //   element: <Login />,
+        // },
+        // {
+        //   path: "/register",
+        //   element: <Register />,
+        // },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
-const navbar = () => {
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const user = true;
   return (
     <nav>
       <div className="left">
         <a href="/" className="logo">
-          <img src="/logo.png" alt="logo" />
+          <img src="/logo.png" alt="" />
           <span>LamaEstate</span>
         </a>
         <a href="/">Home</a>
@@ -15,14 +19,34 @@ const navbar = () => {
         <a href="/">Agents</a>
       </div>
       <div className="right">
+        {/* {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>Rohit Mathur</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <> */}
         <a href="/">Sign in</a>
         <a href="/" className="register">
           Sign up
         </a>
+        {/* </>
+        )} */}
         <div className="menuIcon">
-          <img src="/menu.png" alt="" />
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
         </div>
-        <div className="menu">
+        <div className={open ? "menu active" : "menu"}>
           <a href="/">Home</a>
           <a href="/">About</a>
           <a href="/">Contact</a>
@@ -33,6 +57,6 @@ const navbar = () => {
       </div>
     </nav>
   );
-};
+}
 
-export default navbar;
+export default Navbar;
